17fc0802c84a4461b9436eba21e1138b161ee0a0
im-madylin <im.madylin@gmail.com>
2023-10-26 00:56:38 +0900

    feat: 붕어빵 디스플레이판 추가

---
+++ frontend/src/components/MainPage/Display.js
@@ -0,0 +1,11 @@
+import board from "images/bg_board.png";
+
+const Display = () => {
+  return (
+    <div>
+      <img className="w-[413px] h-[261px]" src={board} alt="display" />
+    </div>
+  );
+};
+
+export default Display;
