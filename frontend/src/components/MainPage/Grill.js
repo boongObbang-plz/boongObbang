d634a7dab541ee6661fcad373ebd0213e3ee1ebb
im-madylin <im.madylin@gmail.com>
2023-10-26 00:56:16 +0900

    feat: 붕어빵 판 추가

---
+++ frontend/src/components/MainPage/Grill.js
@@ -0,0 +1,15 @@
+import frame from "images/bg_frame.png";
+
+const Grill = () => {
+  return (
+    <div>
+      <img
+        className="w-[317px] h-[70px] justify-center content-center"
+        src={frame}
+        alt="grill"
+      />
+    </div>
+  );
+};
+
+export default Grill;
