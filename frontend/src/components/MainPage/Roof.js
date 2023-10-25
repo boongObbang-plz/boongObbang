82b003de0609956a83d4f6eab17e129db575ab4b
im-madylin <im.madylin@gmail.com>
2023-10-25 22:39:18 +0900

    fix: 경로 수정

--- frontend/src/components/MainPage/Roof.js
+++ frontend/src/components/MainPage/Roof.js
@@ -1,11 +1,11 @@
-import redRoof from '../../images/bg_roof_red.png'
+import redRoof from "images/bg_roof_red.png";
 
 const Roof = () => {
-    return (
-        <div>
-            <img src={redRoof}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src={redRoof} alt="roof" />
+    </div>
+  );
+};
 
 export default Roof;
