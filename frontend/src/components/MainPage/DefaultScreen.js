6979ccf7ee68ba0a3ae89a2238178102b193c584
Hahyeon Lee <im.madylin@gmail.com>
2023-10-25 20:39:18 +0900

    feat: 붕어빵 지붕 적용중

---
+++ frontend/src/components/MainPage/DefaultScreen.js
@@ -0,0 +1,11 @@
+import Roof from "/app/src/components/Roof";
+
+const DefaultScreen = () => {
+    return (
+        <>
+            <Roof />
+        </>
+    )
+}
+
+export default DefaultScreen;
