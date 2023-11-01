fe0735e5420d27663463195a5a3d2def6196600c
dlwndms0812 <dlwndms0812@naver.com>
2023-11-01 16:07:15 +0900

    feat: Custom Exception 및 ExceptionHandler 구현

---
+++ backend/src/main/java/com/example/boongObbang/exception/CustomExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.example.boongObbang.exception;
+
+import com.example.boongObbang.exception.exceptions.DeletedMessageException;
+import com.example.boongObbang.exception.exceptions.ExpireAccessTokenException;
+import com.example.boongObbang.exception.exceptions.InvalidAccessTokenException;
+import com.example.boongObbang.exception.exceptions.NoExistMessageException;
+import com.example.boongObbang.response.CustomResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity DeletedMessageExceptionHandler(DeletedMessageException e) {
+		return new ResponseEntity(
+			CustomResponse.response(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
+			HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity ExpiredAccessTokenExceptionHandler(ExpireAccessTokenException e) {
+		return new ResponseEntity(
+			CustomResponse.response(HttpStatus.UNAUTHORIZED.value(), e.getMessage()),
+			HttpStatus.UNAUTHORIZED);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity InvalidAccessTokenExceptionHandler(InvalidAccessTokenException e) {
+		return new ResponseEntity(
+			CustomResponse.response(HttpStatus.UNAUTHORIZED.value(), e.getMessage()),
+			HttpStatus.UNAUTHORIZED);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity NoExistMessageExceptionHandler(NoExistMessageException e) {
+		return new ResponseEntity(
+			CustomResponse.response(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
+			HttpStatus.BAD_REQUEST);
+	}
+}
---
+++ backend/src/main/java/com/example/boongObbang/exception/exceptions/InvalidAccessTokenException.java
@@ -0,0 +1,8 @@
+package com.example.boongObbang.exception.exceptions;
+
+public class InvalidAccessTokenException extends RuntimeException {
+	public InvalidAccessTokenException(String message) {
+		super(message);
+	}
+
+}
