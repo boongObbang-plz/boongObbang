f7effe896c993c8686d6574b6cc935fbb70408d1
정성은 <soc06241@naver.com>
2023-11-05 23:59:31 +0900

    fix: useState 위치 수정

--- frontend/src/components/SettingPage/DefaultScreen.js
+++ frontend/src/components/SettingPage/DefaultScreen.js
@@ -3,12 +3,21 @@ import Header   from './Header'
 import Name     from './Name'
 import PreView  from './PreView'
 import StartButton   from './StartButton'
+import { useState } from 'react'
+
 const DefaultScreen = () => {
+    const [nn, setName] = useState("")
+    const [nameError, setNameError] = useState("")
+    const [colorValue, setColorValue] = useState(0)
+	const [lightValue, setLightValue] = useState(0)
+
     return (
         <div className="w-[393px] h-[1060px] flex flex-col">
             <Header/>
-            <Name/>
-            <PreView/>
+            <Name setName={setName} setNameError={setNameError}/>
+            <div className="flex justify-start pl-[10%] text-[20px] font-normal">{nameError}</div>
+            <PreView setColorValue={setColorValue} setLightValue={setLightValue}/>
+            <button onClick={() => {console.log(nn)}}>이름 확인</button>
             <StartButton/>
         </div>
     )
