f48e448ec0f4fd7d562c9cc73c56c6174f81fc6c
dlwndms0812 <dlwndms0812@naver.com>
2023-11-08 16:00:19 +0900

    test: 마차 만들기 service 테스트 코드 추가 및 수정

--- backend/src/test/java/com/example/boongObbang/service/SettingServiceTest.java
+++ backend/src/test/java/com/example/boongObbang/service/SettingServiceTest.java
@@ -1,9 +1,14 @@
 package com.example.boongObbang.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.example.boongObbang.dto.CreateSettingRequestDto;
 import com.example.boongObbang.entity.User;
 import com.example.boongObbang.repository.SettingRepository;
 import com.example.boongObbang.repository.UserRepository;
+import com.example.boongObbang.response.ResponseMessage;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,15 +42,35 @@ public class SettingServiceTest {
 		userRepository.save(user);
 
 		//when
-		CreateSettingRequestDto createSettingRequestDto = CreateSettingRequestDto.builder()
-			.name("주은이네 붕어빵")
-			.color(1)
-			.light(1).build();
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
 
 		//then
-
 		assertDoesNotThrow(() -> {
 			settingService.createSetting(createSettingRequestDto, email);
 		});
+	}
+
+	@Test
+	@DisplayName("마차 만들기 실패 테스트(이메일 존재x)")
+	public void fail_email() {
+		//given
+		String email = "settingserviceNouser@test.com";
+
+		//when
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			settingService.createSetting(createSettingRequestDto, email);
+		});
+		//then
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_EMAIL);
 	}
 }
