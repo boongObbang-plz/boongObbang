5af88877a75e45c438b9bb756867e67c3035ac48
Hahyeon Lee <im.madylin@gmail.com>
2023-11-14 15:16:59 +0900

    fix: conflict 해결

--- frontend/src/components/LoginPage/Message.js
+++
@@ -1,7 +0,0 @@
-const Message = ({ message }) => {
-    return (
-        <div className="flex w-[100%] text-[18px] pb-[2px] text-justify">{ message }</div>
-    )
-}
-
-export default Message
--- frontend/src/components/LoginPage/MessageTitle.js
+++
@@ -1,7 +0,0 @@
-const MessageTitle = ({ title }) => {
-    return (
-        <div className="flex w-[100%] text-[18px] font-bold pb-[4px]">{title}</div>
-    )
-}
-
-export default MessageTitle
