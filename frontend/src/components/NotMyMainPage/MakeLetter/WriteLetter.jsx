0f99c908c5a5d37dea29c6256a70bbf576c03178
Hahyeon Lee <im.madylin@gmail.com>
2023-11-14 21:20:47 +0900

    style: 구조 수정

--- frontend/src/components/NotMyMainPage/MakeLetter/WriteLetter.jsx
+++ frontend/src/components/NotMyMainPage/MakeLetter/WriteLetter.jsx
@@ -20,18 +20,35 @@ const WriteLetter = () => {
         console.log(writeLetter.from);
     }
 
+    // todo: submit button, 글자수 체크
+
     return (
     <>
         <Top />
         <div className="h-[100%] flex flex-col items-start justify-between text-left">
             <div className="w-72 flex justify-between mt-2">
                 <div className="w-20 flex justify-center text-lg ">To</div>
-                <input type="text" className="w-52 h-8  border-l-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg focus:outline-0" onChange={onChangeTo} value={writeLetter.to} />
+                <input 
+                    type="text"
+                    className="w-52 h-8  border-l-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg focus:outline-0"
+                    onChange={onChangeTo}
+                    value={writeLetter.to}
+                />
             </div>
-            <textarea type="text" className="w-72 h-96 border-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg  mt-4 focus:outline-0" onChange={onChangeMessage} value={writeLetter.message} />
+            <textarea 
+                type="text"
+                className="w-72 h-96 border-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg  mt-4 focus:outline-0"
+                onChange={onChangeMessage}
+                value={writeLetter.message}
+            />
             <div className="w-72 flex justify-between mt-4">
                 <div className="w-20 flex justify-center text-lg ">From</div>
-                <input type="text" className="w-52 h-8  border-l-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg focus:outline-0" onChange={onChangeFrom} value={writeLetter.from} />
+                <input 
+                    type="text"
+                    className="w-52 h-8  border-l-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg focus:outline-0"
+                    onChange={onChangeFrom}
+                    value={writeLetter.from}
+                />
             </div>
         </div>
     </>
