0a243478799fc31f59b78e13efa06880d014bac1
정성은 <soc06241@naver.com>
2023-11-14 15:09:19 +0900

    fix: recoil install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LoginPage from "components/LoginPage/LoginPage";
 import MainPage from "pages/MainPage";
+import Layout from "components/layout";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/mainpage" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<LoginPage />} index />
+            <Route path="/mainpage" element={<MainPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
