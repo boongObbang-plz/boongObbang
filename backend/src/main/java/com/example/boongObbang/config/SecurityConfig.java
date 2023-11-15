86eabdc22ba996b34af4b13f238e38797eebbae4
dlwndms0812 <dlwndms0812@naver.com>
2023-11-15 14:50:51 +0900

    fix: controller 테스트시 403이 뜨는 오류 해결

--- backend/src/main/java/com/example/boongObbang/config/SecurityConfig.java
+++ backend/src/main/java/com/example/boongObbang/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AllArgsConstructor
-@Configuration(proxyBeanMethods = true)
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -29,18 +29,22 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
 		JwtAuthorizationFilter jwtAuthorizationFilter;
-
+		
 		httpSecurity
+			.cors(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests((request) -> request.requestMatchers("/login/**").permitAll()
 				.requestMatchers("/main/**").permitAll()
 				.requestMatchers("/").permitAll()
-				.anyRequest().authenticated());
+				.requestMatchers("/settings").permitAll());
+
 
 		httpSecurity.sessionManagement((session) -> session.sessionCreationPolicy(
 			SessionCreationPolicy.STATELESS));
 
 		httpSecurity.addFilterBefore(new JwtAuthorizationFilter(provider, userRepository), UsernamePasswordAuthenticationFilter.class);
+
+		httpSecurity.formLogin(AbstractHttpConfigurer::disable);
 
 		return httpSecurity.build();
 	}
