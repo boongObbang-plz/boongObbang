38c81ebfcf60dd4d5b875dd4e1b28b14fb9d52f8
Hahyeon Lee <im.madylin@gmail.com>
2023-11-15 20:47:49 +0900

    fix: 조명 위치 수정

--- frontend/src/components/MainPage/Light.jsx
+++ frontend/src/components/MainPage/Light.jsx
@@ -2,7 +2,7 @@ import LightBulbs from "@components/MainPage/LightBulbs";
 
 const Light = ({ selectedIdx }) => {
   return (
-    <div className="relative bottom-[30vw] right-1 ">
+    <div className="relative bottom-[30vw] right-2 ">
       <div className="w-[102%] h-5 rounded-[80%] -rotate-12 shadow-light">
         <LightBulbs selectedIdx={selectedIdx} secondLine={false} />
       </div> 
