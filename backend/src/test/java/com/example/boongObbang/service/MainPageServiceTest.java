8b668bc52c17a959e35575776441a7573a134c45
dlwndms0812 <dlwndms0812@naver.com>
2023-11-16 17:23:47 +0900

    test: 나의 메인페이지 조회 service 테스트 코드 추가 및 수정

--- backend/src/test/java/com/example/boongObbang/service/MainPageServiceTest.java
+++ backend/src/test/java/com/example/boongObbang/service/MainPageServiceTest.java
@@ -2,6 +2,7 @@ package com.example.boongObbang.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.boongObbang.dto.CreateSettingRequestDto;
 import com.example.boongObbang.dto.MainPageResponseDto;
@@ -9,6 +10,7 @@ import com.example.boongObbang.entity.Message;
 import com.example.boongObbang.entity.User;
 import com.example.boongObbang.repository.MessageRepository;
 import com.example.boongObbang.repository.UserRepository;
+import com.example.boongObbang.response.ResponseMessage;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,7 +38,7 @@ public class MainPageServiceTest {
 	@DisplayName("나의 메인페이지 조회 성공 테스트")
 	public void getMyMainPage() {
 		//given
-		String email = "settingservice@test.com";
+		String email = "messageservice@test.com";
 		String provider = "google";
 
 		User user = User.builder()
@@ -86,15 +88,15 @@ public class MainPageServiceTest {
 
 		//then
 		assertDoesNotThrow(() -> {
-			mainPageService.getMyPage(email, provider);
+			mainPageService.getMainPage(email, provider);
 		});
 	}
 
 	@Test
 	@DisplayName("나의 메인페이지 조회 실패 테스트(존재하지 않는 유저)")
-	public void getMyMainPage() {
+	public void getMyMainPageFail() {
 		//given
-		String email = "settingservice@test.com";
+		String email = "messageservice@test.com";
 		String provider = "google";
 
 		User user = User.builder()
@@ -146,11 +148,66 @@ public class MainPageServiceTest {
 		};
 
 		//then
-		assertDoesNotThrow(() -> {
-			ref.mainPageResponseDto = mainPageService.getMainPage(email, "kakao");
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.getMainPage(email, "kakao");
 		});
 
-		assertEquals(ref.mainPageResponseDto.getMessages().size(), 3);
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_EMAIL);
+	}
+
+	@Test
+	@DisplayName("나의 메인페이지 조회 실패 테스트(존재하지 않는 셋팅)")
+	public void getMyMainPageFailSetting() {
+		//given
+		String email = "messageservice@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		//when
+		Message message1 = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		Message message2 = Message.builder()
+			.recipient("받는사람2")
+			.message("메시지~~~~~2")
+			.madeBy("글쓴사람2")
+			.color(2)
+			.ip("ip주소~~~~~~2")
+			.user(user).build();
+
+		Message message3 = Message.builder()
+			.recipient("받는사람3")
+			.message("메시지~~~~~3")
+			.madeBy("글쓴사람3")
+			.color(3)
+			.ip("ip주소~~~~~~3")
+			.user(user).build();
+
+		messageRepository.save(message1);
+		messageRepository.save(message2);
+		messageRepository.save(message3);
+
+		var ref = new Object() {
+			MainPageResponseDto mainPageResponseDto;
+		};
+
+		//then
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.getMainPage(email, provider);
+		});
+
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_SETTING);
 	}
 
 }
