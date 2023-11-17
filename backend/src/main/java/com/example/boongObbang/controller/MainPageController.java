b7af3d3d30249a4ece8c6feca9867e2fef483d11
dlwndms0812 <dlwndms0812@naver.com>
2023-11-17 16:41:51 +0900

    feat: 편지 삭제 controller 구현

--- backend/src/main/java/com/example/boongObbang/controller/MainPageController.java
+++ backend/src/main/java/com/example/boongObbang/controller/MainPageController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,4 +47,18 @@ public class MainPageController {
 		return new ResponseEntity(CustomResponse.response(HttpStatus.OK.value(), ResponseMessage.SUCCESS, linkResponseDto),
 			HttpStatus.OK);
 	}
+
+	@PatchMapping("/message/{idx}")
+	public ResponseEntity deleteMessage(@PathVariable("idx") Long idx, @RequestHeader("Authorization") String token) {
+		String email = jwtProvider.getEmail(token);
+		String provider = jwtProvider.getProvider(token);
+
+		mainPageService.deleteMessage(email, provider, idx);
+
+		return new ResponseEntity(CustomResponse.response(HttpStatus.OK.value(), ResponseMessage.SUCCESS),
+			HttpStatus.OK);
+	}
+
+
+	//TODO: 편지 읽기 기능에서 d_day체크 controller에서 하기
 }
