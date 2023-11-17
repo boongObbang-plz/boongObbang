95437fcdb304d21d53383dcf5e91e59ce2e74c69
dlwndms0812 <dlwndms0812@naver.com>
2023-11-17 15:02:47 +0900

    test: 편지 읽기 service 테스트 코드 작성

--- backend/src/test/java/com/example/boongObbang/service/MainPageServiceTest.java
+++ backend/src/test/java/com/example/boongObbang/service/MainPageServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.boongObbang.dto.CreateSettingRequestDto;
 import com.example.boongObbang.dto.MainPageResponseDto;
+import com.example.boongObbang.dto.ReadMessageResponseDto;
 import com.example.boongObbang.entity.Message;
 import com.example.boongObbang.entity.User;
 import com.example.boongObbang.repository.MessageRepository;
@@ -84,7 +85,6 @@ public class MainPageServiceTest {
 		messageRepository.save(message1);
 		messageRepository.save(message2);
 		messageRepository.save(message3);
-
 
 		//then
 		assertDoesNotThrow(() -> {
@@ -254,4 +254,316 @@ public class MainPageServiceTest {
 		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_EMAIL);
 	}
 
+	@Test
+	@DisplayName("편지 읽기 성공 테스트")
+	public void successReadMessage() {
+		//given
+		String email = "readservice@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		Message message1 = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		Message message2 = Message.builder()
+			.recipient("받는사람2")
+			.message("메시지~~~~~2")
+			.madeBy("글쓴사람2")
+			.color(2)
+			.ip("ip주소~~~~~~2")
+			.user(user).build();
+
+		Message message3 = Message.builder()
+			.recipient("받는사람3")
+			.message("메시지~~~~~3")
+			.madeBy("글쓴사람3")
+			.color(3)
+			.ip("ip주소~~~~~~3")
+			.user(user).build();
+
+		messageRepository.save(message1);
+		messageRepository.save(message2);
+		messageRepository.save(message3);
+
+		//when
+		var ref = new Object() {
+			ReadMessageResponseDto readMessageResponseDto;
+		};
+
+		assertDoesNotThrow(() -> {
+			ref.readMessageResponseDto = mainPageService.readMessage(email, provider, 1);
+		});
+
+		//then
+		assertEquals(ref.readMessageResponseDto.getTo(), message1.getRecipient());
+	}
+
+	@Test
+	@DisplayName("편지 읽기 성공 테스트(이전 메세지가 지워져도 불러오는 경우)")
+	public void successReadMessageDelete() {
+		//given
+		String email = "readservicedelete@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		Message message1 = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		Message message2 = Message.builder()
+			.recipient("받는사람2")
+			.message("메시지~~~~~2")
+			.madeBy("글쓴사람2")
+			.color(2)
+			.ip("ip주소~~~~~~2")
+			.user(user).build();
+
+		Message message3 = Message.builder()
+			.recipient("받는사람3")
+			.message("메시지~~~~~3")
+			.madeBy("글쓴사람3")
+			.color(3)
+			.ip("ip주소~~~~~~3")
+			.user(user).build();
+
+		messageRepository.save(message1);
+		messageRepository.save(message2);
+		messageRepository.save(message3);
+
+		messageRepository.delete(message2);
+
+		//when
+		var ref = new Object() {
+			ReadMessageResponseDto readMessageResponseDto;
+		};
+
+		assertDoesNotThrow(() -> {
+			mainPageService.readMessage(email, provider, 3);
+		});
+
+		//then
+		assertEquals(ref.readMessageResponseDto.getTo(), message3.getRecipient());
+	}
+
+	@Test
+	@DisplayName("편지 읽기 실패 테스트(존재하지 않는 유저인 경우)")
+	public void failReadMessageUser() {
+		//given
+		String email = "readserviceuser@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		Message message1 = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		messageRepository.save(message1);
+
+		//when
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.readMessage(email, provider, 1);
+		});
+
+		//then
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_EMAIL);
+	}
+
+	@Test
+	@DisplayName("편지 읽기 실패 테스트(존재하지 않는 idx인 경우)")
+	public void failReadMessageIdx() {
+		//given
+		String email = "readserviceidx@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		//when
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.readMessage(email, provider, 25);
+		});
+
+		//then
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_MESSAGE);
+	}
+
+	@Test
+	@DisplayName("편지 읽기 실패 테스트(읽고자하는 편지의 주인이 다른 경우)")
+	public void failReadMessageNotUser() {
+		//given
+		String email = "readservicenotuser@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		String user2_email = "user2@test.com";
+
+		User user2 = User.builder()
+			.email(user2_email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user2);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		Message message = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		messageRepository.save(message);
+
+		//when
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.readMessage(user2_email, provider, 1);
+		});
+
+		//then
+		assertEquals(throwable.getMessage(), ResponseMessage.NO_EXIST_MESSAGE);
+	}
+
+	@Test
+	@DisplayName("편지 읽기 실패 테스트(삭제된 편지인 경우)")
+	public void failReadMessageDelete() {
+		//given
+		String email = "readservicedeleter@test.com";
+		String provider = "google";
+
+		User user = User.builder()
+			.email(email)
+			.uuid(UUID.randomUUID().toString())
+			.provider(provider).build();
+
+		userRepository.save(user);
+
+		CreateSettingRequestDto createSettingRequestDto = new CreateSettingRequestDto();
+
+		createSettingRequestDto.setName("주은이네 붕어빵");
+		createSettingRequestDto.setColor(1);
+		createSettingRequestDto.setLight(1);
+
+		settingService.createSetting(createSettingRequestDto, email, provider);
+
+		Message message = Message.builder()
+			.recipient("받는사람")
+			.message("메시지~~~~~")
+			.madeBy("글쓴사람")
+			.color(1)
+			.ip("ip주소~~~~~~")
+			.user(user).build();
+
+		messageRepository.save(message);
+
+		Message message2 = Message.builder()
+			.recipient("받는사람2")
+			.message("메시지~~~~~2")
+			.madeBy("글쓴사람2")
+			.color(1)
+			.ip("ip주소~~~~~~2")
+			.user(user).build();
+
+		messageRepository.save(message2);
+
+		Message message3 = Message.builder()
+			.recipient("받는사람3")
+			.message("메시지~~~~~3")
+			.madeBy("글쓴사람3")
+			.color(1)
+			.ip("ip주소~~~~~~3")
+			.user(user).build();
+
+		messageRepository.save(message3);
+
+		messageRepository.delete(message2);
+
+		//when
+		Throwable throwable = assertThrows(RuntimeException.class, () -> {
+			mainPageService.readMessage(email, provider, 2);
+		});
+
+		//then
+		assertEquals(throwable.getMessage(), ResponseMessage.DELETED_MESSAGE);
+	}
 }
