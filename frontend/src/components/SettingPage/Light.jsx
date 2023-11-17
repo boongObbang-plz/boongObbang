3c1b412bdea384419d16795ce06bb27a8f06fd8f
정성은 <soc06241@naver.com>
2023-11-17 17:07:23 +0900

    fix: 색상 변경

--- frontend/src/components/SettingPage/Light.jsx
+++ frontend/src/components/SettingPage/Light.jsx
@@ -12,7 +12,7 @@ const Light = ({light, index}) => {
         <div className="mx-2 mt-1" onClick={() => onClickRoof(index)}>
             <div
             className="flex  items-center justify-center w-[132px] h-[132px] bg-[#FFFFFF] rounded-[30px]"
-            style={{ "borderWidth": "7px", "borderColor": index === lightColor.light ? "#192655" : "#FFFFFF"}}
+            style={{ "borderWidth": "7px", "borderColor": index === lightColor.light ? "#B3ABAE" : "#FFFFFF"}}
             >
                 <img src={light[0]} alt="light_icon" />
             </div>
--- frontend/src/components/SettingPage/Roof.jsx
+++ frontend/src/components/SettingPage/Roof.jsx
@@ -12,7 +12,7 @@ const Roof = ({index}) => {
         <div className="mx-2 mt-1" onClick={() => onClickRoof(index)}>
             <div
             className="flex items-center justify-center w-[132px] h-[132px] bg-[#DDDCDC] rounded-[30px]"
-            style={{ "borderWidth": "7px", "borderColor": index === roofColor.color ? "#192655" : "#FFFFFF",
+            style={{ "borderWidth": "7px", "borderColor": index === roofColor.color ? "#B3ABAE" : "#FFFFFF",
                 "background": index === 0 ? "#DF6F5F" : index === 1 ? "#F48E35" : index === 2 ? "#9FB433" : "#8FBDDF"}}
             >
             </div>
