e1064147756b70ae32aa5c5ee5e0ed13e8893b1c
정성은 <soc06241@naver.com>
2023-11-18 22:29:31 +0900

    feat: symbol

--- frontend/src/components/LoginPage/GoogleLoginButton.jsx
+++ frontend/src/components/LoginPage/GoogleLoginButton.jsx
@@ -1,9 +1,12 @@
-
+import googleSymbol from "/images/google_symbol.png"
 
 const GoogleLoginButton = () => {
     return (
-        <div className="w-[90%] h-[60px] bg-[#FFFFFF] rounded-[12px] text-center text-[30px] text-[#191919] flex justify-center mb-[2%]">
-            <button>구글 로그인</button>
+        <div className="w-[70%] h-[60px] bg-[#FFFFFF] rounded-[12px] text-center text-[25px] text-[#191919] flex justify-center items-center mb-[2%] font-sans">
+            <button>
+            <img  className="absolute left-[20%] w-[40px] h-[40px] "src={googleSymbol} alt="google_symbol" />
+                구글 로그인
+            </button>
         </div>
     )
 }
--- frontend/src/components/LoginPage/SocialKakao.jsx
+++ frontend/src/components/LoginPage/SocialKakao.jsx
@@ -1,7 +1,7 @@
-
+import kakaoSymbol from "/images/kakao_symbol.png"
 
 const SocialKakao = () => {
-  const Rest_api_key = "fc790fdb3ffa52667e5fc35e231fc13b"; //REST API KEY
+  const Rest_api_key = ""; //REST API KEY
   const redirect_uri = "http://localhost:5173/login/oauth2/code/kakao"; //Redirect URI
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
@@ -9,11 +9,12 @@ const SocialKakao = () => {
     window.location.href = kakaoURL;
   };
   return (
-    <div className="w-[90%] h-[60px] bg-[#FEE500] rounded-[12px] text-center text-[30px] text-[#191919] flex justify-center">
-      <button onClick={handleLogin}>
-        <img src="" alt="" />
-        카카오 로그인
-      </button>
+    <div 
+    className="w-[70%] h-[60px] bg-[#FEE500] rounded-[12px] text-center text-[25px] text-[#191919] flex justify-center items-center font-sans"
+    onClick={handleLogin}
+    >
+      <img className="absolute left-[20%] w-[45px] h-[45px]" src={kakaoSymbol} alt="kakao_symbol" />
+      카카오 로그인
     </div>
   );
 };
