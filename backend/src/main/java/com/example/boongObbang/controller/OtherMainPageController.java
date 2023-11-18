1f33c376f999b72e1765f31824c7dc9c30e9b95c
dlwndms0812 <dlwndms0812@naver.com>
2023-11-18 14:57:07 +0900

    fix: requestbody 어노테이션 미부착 에러 해결

--- backend/src/main/java/com/example/boongObbang/controller/OtherMainPageController.java
+++ backend/src/main/java/com/example/boongObbang/controller/OtherMainPageController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,8 @@ public class OtherMainPageController {
 	}
 
 	@PostMapping("/{uuid}")
-	public ResponseEntity writeMessage(@PathVariable("uuid") String uuid, WriteMessageResponseDto writeMessageResponseDto, HttpServletRequest request) {
+	public ResponseEntity writeMessage(@PathVariable("uuid") String uuid, @RequestBody WriteMessageResponseDto writeMessageResponseDto, HttpServletRequest request) {
+
 		mainPageService.writeMessage(writeMessageResponseDto, uuid, request.getRemoteAddr());
 
 		HttpHeaders httpHeaders = new HttpHeaders();
