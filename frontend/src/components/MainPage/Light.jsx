4add14cbef20261246bd4a2bc1e5a2ce9f1c0538
Hahyeon Lee <im.madylin@gmail.com>
2023-11-24 14:09:58 +0900

    fix: 최대 사이즈 이후 조명 높이 수정

--- frontend/src/components/MainPage/Light.jsx
+++ frontend/src/components/MainPage/Light.jsx
@@ -2,7 +2,7 @@ import LightBulbs from "@components/MainPage/LightBulbs";
 
 const Light = ({ selectedIdx }) => {
   return (
-    <div className="relative mx-auto bottom-[30vw] right-1">
+    <div className="relative mx-auto bottom-[30vw] right-1 min-[700px]:bottom-[160px]">
       <div className="absolute w-[102%] h-6 rounded-[80%] -rotate-12 shadow-light">
         <LightBulbs selectedIdx={selectedIdx} secondLine={false} />
       </div> 
--- frontend/src/pages/NotMyMainPage.jsx
+++ frontend/src/pages/NotMyMainPage.jsx
@@ -2,7 +2,6 @@ import Top from "@components/MainPage/Top";
 import Display from "@components/MainPage/Display";
 import Bottom from "@components/NotMyMainPage/Bottom";
 import fakeData from "@components/MainPage/fake_api_data.json";
-import Title from "@components/MainPage/Title";
 
 const NotMyMainPage = () => {
   if (fakeData.status === 200) console.log("api 연동 성공");
