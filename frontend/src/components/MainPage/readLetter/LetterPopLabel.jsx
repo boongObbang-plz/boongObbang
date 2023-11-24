12fbab3a2488c5d18adfaafd4630b88b21620d09
Hahyeon Lee <im.madylin@gmail.com>
2023-11-24 14:55:11 +0900

    fix: 편지읽기, 편지쓰기 팝업 스타일 맞춤

--- frontend/src/components/MainPage/readLetter/LetterPopLabel.jsx
+++ frontend/src/components/MainPage/readLetter/LetterPopLabel.jsx
@@ -1,12 +1,8 @@
-import LetterPopTextArea from "@components/MainPage/readLetter/LetterPopTextArea";
-
 const LetterPopLabel = ({ label, text }) => {
   return (
-    <div className="flex items-center my-[5%]">
-      <div className="w-[35%]">{label}</div>
-      <div className="w-[65%]">
-        <LetterPopTextArea text={text} />
-      </div>
+    <div className="h-full w-full flex justify-center mt-2">
+      <div className="w-1/3 flex justify-center items-center text-lg min-[400px]:text-[17px] min-[500px]:text-[20px] min-[600px]:text-[24px]">{label}</div>
+      <div className="flex h-8 w-3/4 items-center border-l-[25px] border-[#DDDCDC] bg-[#DDDCDC] rounded-lg text-md min-[400px]:text-[16px] min-[500px]:text-[19px] min-[600px]:text-[23px]">{text}</div>
     </div>
   );
 };
