ca3039fe678684d957b8eb749e41a43cacaccd4d
dlwndms0812 <dlwndms0812@naver.com>
2023-11-24 22:22:32 +0900

    fix: setAllowedOriginPatterns로 변경

--- backend/src/main/java/com/example/boongObbang/config/SecurityConfig.java
+++ backend/src/main/java/com/example/boongObbang/config/SecurityConfig.java
@@ -42,9 +42,8 @@ public class SecurityConfig {
 				@Override
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration config = new CorsConfiguration();
-					config.setAllowedOrigins(Collections.singletonList("*"));
+					config.setAllowedOriginPatterns(Collections.singletonList("*"));
 					config.setAllowedMethods(Collections.singletonList("*"));
-					config.setAllowCredentials(true);
 					config.setAllowedHeaders(Collections.singletonList("*"));
 					config.setExposedHeaders(Collections.singletonList("*"));
 					config.setMaxAge(3600L);
