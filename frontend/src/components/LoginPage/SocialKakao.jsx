a902a3dfa8fd4a839c4824fbbcec3b472b9bd166
Seongeun Jeong <seonjeon@student.42seoul.kr>
2023-11-27 16:03:44 +0900

    style: css 수정

--- frontend/src/components/LoginPage/SocialKakao.jsx
+++ frontend/src/components/LoginPage/SocialKakao.jsx
@@ -10,11 +10,11 @@ const SocialKakao = () => {
   };
   return (
     <div 
-    className="w-[70%] h-[60px] bg-[#FEE500] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative font-sans"
+    className="w-[70%] h-[50px] bg-[#FEE500] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative font-sans mb-[2%] font-sans flex items-center justify-center"
     onClick={handleLogin}
     >
-      <img className="absolute left-[10%] top-[20%] w-[36px] h-[36px]" src={kakaoSymbol} alt="kakao_symbol" />
-      <div className="flex h-[100%] justify-center items-center">카카오 로그인</div>
+      <img className="absolute left-[10%] w-[28px] h-[28px] min-[400]:w-[30px] min-[400]:h-[30px]" src={kakaoSymbol} alt="kakao_symbol" />
+      <div className="flex justify-center items-center">카카오 로그인</div>
     </div>
   );
 };
--- frontend/src/components/LoginPage/Title.jsx
+++ frontend/src/components/LoginPage/Title.jsx
@@ -3,10 +3,12 @@ import NoticeButton from "@components/LoginPage/NoticeButton"
 const Title = () => {
     return (
         <div className="w-full text-title-color pt-[10px] pb-[2%] text-center">
-            <NoticeButton />
-            <div className="flex justify-center text-[40px] min-[400px]:text-[43px] min-[500px]:text-[46px] min-[600px]:text-[50px] mb-[3%]">
-                붕어빵을 부탁해
-            </div> 
+            <div className="relative flex justify-center">            
+                <div className="flex justify-center items-center text-[40px] min-[400px]:text-[43px] min-[500px]:text-[46px] min-[600px]:text-[50px]">
+                    붕어빵을 부탁해
+                </div>
+                <NoticeButton />
+            </div>
             <div className="text-[20px] min-[400px]:text-[22px] min-[500px]:text-[24px] min-[600px]:text-[26px]">
                 붕어빵처럼 따뜻한 편지를 나누어요
             </div>
