c84d3487a0869ce751987d855d5f42f87791db0c
Seongeun Jeong <seonjeon@student.42seoul.kr>
2023-11-27 19:37:19 +0900

    fix: onclick 이벤트 추가

--- frontend/src/components/LoginPage/GoogleLoginButton.jsx
+++ frontend/src/components/LoginPage/GoogleLoginButton.jsx
@@ -9,7 +9,7 @@ const handleLogin = () => {
 
 const GoogleLoginButton = () => {
     return (
-        <div className="w-[70%] h-[50px] bg-[#FFFFFF] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative mb-[2%] font-sans">
+        <div className="w-[70%] h-[50px] bg-[#FFFFFF] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative mb-[2%] font-sans" onClick={handleLogin}>
             <img  className="absolute left-[10%] top-[22%] w-[26px] h-[26px] "src={googleSymbol} alt="google_symbol" />
             <div className="flex h-[100%] justify-center items-center">구글 로그인</div>
         </div>
