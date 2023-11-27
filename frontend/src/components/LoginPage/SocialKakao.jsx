3386ac3c21129a9a82f1a277b47a9288da72305e
Seongeun Jeong <seonjeon@student.42seoul.kr>
2023-11-27 17:06:50 +0900

    fix: px 단위 추가

--- frontend/src/components/LoginPage/SocialKakao.jsx
+++ frontend/src/components/LoginPage/SocialKakao.jsx
@@ -13,7 +13,7 @@ const SocialKakao = () => {
     className="w-[70%] h-[50px] bg-[#FEE500] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative font-sans mb-[2%] font-sans flex items-center justify-center"
     onClick={handleLogin}
     >
-      <img className="absolute left-[10%] w-[28px] h-[28px] min-[400]:w-[30px] min-[400]:h-[30px]" src={kakaoSymbol} alt="kakao_symbol" />
+      <img className="absolute left-[10%] w-[28px] h-[28px] min-[400px]:w-[30px] min-[400px]:h-[30px]" src={kakaoSymbol} alt="kakao_symbol" />
       <div className="flex justify-center items-center">카카오 로그인</div>
     </div>
   );
