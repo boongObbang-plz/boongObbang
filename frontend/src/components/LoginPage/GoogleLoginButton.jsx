126a34a7bd9283c30f3951f4a9bbaa6c084223ec
im-madylin <im.madylin@gmail.com>
2024-05-24 14:17:07 +0900

    fix: url localhost > boongobbang.site 변경

--- frontend/src/components/LoginPage/GoogleLoginButton.jsx
+++ frontend/src/components/LoginPage/GoogleLoginButton.jsx
@@ -1,19 +1,29 @@
-import googleSymbol from "/images/google_symbol.png"
+import googleSymbol from "/images/google_symbol.png";
 const client_id = import.meta.env.VITE_GOOGLE_API_ID;
-const redirect_uri = "http://localhost:5173/login/oauth2/code/google";
-const googleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`
+// const redirect_uri = "http://localhost:5173/login/oauth2/code/google";
+const redirect_uri = "https://boongobbang.site/login/oauth2/code/google";
+const googleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
 
 const handleLogin = () => {
-    window.location.href = googleURL;
-}
+  window.location.href = googleURL;
+};
 
 const GoogleLoginButton = () => {
-    return (
-        <div className="w-[70%] h-[50px] bg-[#FFFFFF] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative mb-[2%] font-sans" onClick={handleLogin}>
-            <img  className="absolute left-[10%] top-[22%] w-[26px] h-[26px] "src={googleSymbol} alt="google_symbol" />
-            <div className="flex h-[100%] justify-center items-center">구글 로그인</div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="relative mb-[2%] h-[50px] w-[70%] rounded-[12px] bg-[#FFFFFF] text-center font-sans text-[16px] text-[#191919] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px]"
+      onClick={handleLogin}
+    >
+      <img
+        className="absolute left-[10%] top-[22%] h-[26px] w-[26px] "
+        src={googleSymbol}
+        alt="google_symbol"
+      />
+      <div className="flex h-[100%] items-center justify-center">
+        구글 로그인
+      </div>
+    </div>
+  );
+};
 
-export default GoogleLoginButton
+export default GoogleLoginButton;
--- frontend/src/components/LoginPage/SocialKakao.jsx
+++ frontend/src/components/LoginPage/SocialKakao.jsx
@@ -1,20 +1,25 @@
-import kakaoSymbol from "/images/kakao_symbol.png"
+import kakaoSymbol from "/images/kakao_symbol.png";
 
 const SocialKakao = () => {
   const Rest_api_key = import.meta.env.VITE_KAKAO_REST_API_KEY; //REST API KEY
-  const redirect_uri = "http://localhost:5173/login/oauth2/code/kakao"; //Redirect URI
+  // const redirect_uri = "http://localhost:5173/login/oauth2/code/kakao"; //Redirect URI
+  const redirect_uri = "https://boongobbang.site/login/oauth2/code/kakao";
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
   return (
-    <div 
-    className="w-[70%] h-[50px] bg-[#FEE500] rounded-[12px] text-center text-[16px] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px] text-[#191919] relative font-sans mb-[2%] font-sans flex items-center justify-center"
-    onClick={handleLogin}
+    <div
+      className="relative mb-[2%] flex h-[50px] w-[70%] items-center justify-center rounded-[12px] bg-[#FEE500] text-center font-sans font-sans text-[16px] text-[#191919] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px]"
+      onClick={handleLogin}
     >
-      <img className="absolute left-[10%] w-[28px] h-[28px] min-[400px]:w-[30px] min-[400px]:h-[30px]" src={kakaoSymbol} alt="kakao_symbol" />
-      <div className="flex justify-center items-center">카카오 로그인</div>
+      <img
+        className="absolute left-[10%] h-[28px] w-[28px] min-[400px]:h-[30px] min-[400px]:w-[30px]"
+        src={kakaoSymbol}
+        alt="kakao_symbol"
+      />
+      <div className="flex items-center justify-center">카카오 로그인</div>
     </div>
   );
 };
