ceb8f6abf4a0f09ff0ba4f63e0751d5bcde10c45
jueun <57864944+dlwndms0812@users.noreply.github.com>
2024-05-24 16:32:06 +0900

    fix: KakaoProfileDto의 profile json 내용 추가

--- backend/src/main/java/com/example/boongObbang/dto/KakaoProfileDto.java
+++ backend/src/main/java/com/example/boongObbang/dto/KakaoProfileDto.java
@@ -39,6 +39,7 @@ public class KakaoProfileDto {
 			private String thumbnail_image_url;
 			private String profile_image_url;
 			private Boolean is_default_image;
+			private Boolean is_default_nickname;
 		}
 	}
 }
