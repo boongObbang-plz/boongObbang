064aef939a342091bcbf92ee5f82293b50992644
im-madylin <im.madylin@gmail.com>
2024-05-29 16:30:40 +0900

    fix: 구글 로그인 에러 수정

--- frontend/src/components/MainPage/ShareButton.jsx
+++ frontend/src/components/MainPage/ShareButton.jsx
@@ -1,47 +1,51 @@
-import Share from "/images/icon_share.png"
-import { useRecoilState } from "recoil"
-import { modalAlertState, loginState } from "@states/ModalState"
+import { loginState, modalAlertState } from "@states/ModalState";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import Share from "/images/icon_share.png";
 
 const ShareButton = () => {
-    const [ alertOpen, setAlertOpen ] = useRecoilState(modalAlertState);
-    const [login, setLogin] = useRecoilState(loginState);
-    const navigate = useNavigate();
+  const [alertOpen, setAlertOpen] = useRecoilState(modalAlertState);
+  const [login, setLogin] = useRecoilState(loginState);
+  const navigate = useNavigate();
+  const [link, setLink] = useState("");
 
-    const onClickShareButton = () => {
-        const msg = "링크를 클립보드에 복사했어요. 카카오톡이나 SNS로 공유하고 친구들에게 붕어빵 가게를 부탁해봐요🍞";
-
-        fetch(login.url + "/mainpage/link", {
-            method: "GET",
-            headers: {
-                Authorization: login.token
-            },
-        })
-        .then(res => 
-            res.json()
-        )
-        .then(data => {
-            if (data.status !== 200)
-            {
-                setLogin({ isLogin: false, token: "", url: login.url });
-                navigate('/');
-            }
-            let link = data.data.link
-            navigator.clipboard.writeText(link);
-            setAlertOpen({isOpen: true, message:msg})
-            setTimeout(() => {
-                setAlertOpen({isOpen: false, message: ""})
-            }, 3000)
-        })
+  useEffect(() => {
+    if (login.isLogin) {
+      fetch(login.url + "/mainpage/link", {
+        method: "GET",
+        headers: {
+          Authorization: login.token,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status !== 200) {
+            setLogin({ isLogin: false, token: "", url: login.url });
+            navigate("/");
+          }
+          setLink(data.data.link);
+        });
     }
+  }, []);
 
-    return (
-        <div className="w-[10%] mr-[1%]">
-            <button onClick={onClickShareButton}>
-                <img src={Share} alt="share button"/>
-            </button>
-        </div>
-    )
-}
+  const onClickShareButton = () => {
+    const msg =
+      "링크를 클립보드에 복사했어요. 카카오톡이나 SNS로 공유하고 친구들에게 붕어빵 가게를 부탁해봐요🍞";
+    navigator.clipboard.writeText(link);
+    setAlertOpen({ isOpen: true, message: msg });
+    setTimeout(() => {
+      setAlertOpen({ isOpen: false, message: "" });
+    }, 3000);
+  };
 
-export default ShareButton
+  return (
+    <div className="mr-[1%] w-[10%]">
+      <button onClick={onClickShareButton}>
+        <img src={Share} alt="share button" />
+      </button>
+    </div>
+  );
+};
+
+export default ShareButton;
