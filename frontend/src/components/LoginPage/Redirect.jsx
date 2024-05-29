d78c7c0367e33d5e9ec76a76d86fa22e65a11f7e
im-madylin <im.madylin@gmail.com>
2024-05-29 16:20:58 +0900

    fix: 로그인 중 페이지 정렬 이상 수정

--- frontend/src/components/LoginPage/Redirect.jsx
+++ frontend/src/components/LoginPage/Redirect.jsx
@@ -1,42 +1,44 @@
+import { loginState } from "@states//ModalState";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
-import { loginState } from "@states//ModalState";
 import { useRecoilState } from "recoil";
 
 const Redirect = ({ brandPath }) => {
-    const authCode = new URL(document.location.toString()).searchParams.get('code');
-    const navigate = useNavigate();
-    const urlPath = "/login/oauth2/code";
-    const [login, setLogin] = useRecoilState(loginState);
+  const authCode = new URL(document.location.toString()).searchParams.get(
+    "code",
+  );
+  const navigate = useNavigate();
+  const urlPath = "/login/oauth2/code";
+  const [login, setLogin] = useRecoilState(loginState);
 
-    useEffect(() => {
-        //붕어빵 서버 로그인 요청
-        fetch(login.url + urlPath + brandPath, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-               code: authCode, 
-            }),
-        })
-        .then(res => res.json())
-        .then(data => {
-            if (data.status === 200) {
-                setLogin({ isLogin: true, token: data.data.token, url: login.url })
-                navigate('/mainpage')
-            }
-            if (data.status === 201) {
-                setLogin({ isLogin: false, token: data.data.token, url: login.url })
-                navigate('/settings')
-            }
-        })
-    }, [])
-    return (
-        <div className="w-full h-[1000px] flex items-center justify-center">
-            <h1 className="text-[#FFFFFF] text-[50px]">로그인 중입니다 😉</h1>
-        </div>
-    )
-}
+  useEffect(() => {
+    //붕어빵 서버 로그인 요청
+    fetch(login.url + urlPath + brandPath, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: authCode,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 200) {
+          setLogin({ isLogin: true, token: data.data.token, url: login.url });
+          navigate("/mainpage");
+        }
+        if (data.status === 201) {
+          setLogin({ isLogin: false, token: data.data.token, url: login.url });
+          navigate("/settings");
+        }
+      });
+  }, []);
+  return (
+    <div className="flex h-screen w-full items-center justify-center break-keep text-center">
+      <h1 className="text-[50px] text-[#FFFFFF]">로그인 중입니다 😉</h1>
+    </div>
+  );
+};
 
-export default Redirect
+export default Redirect;
