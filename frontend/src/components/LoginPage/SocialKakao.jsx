0ba952d4413099df01dd5712b41c263651d43ceb
eunn43 <soc06241@naver.com>
2024-05-30 13:54:35 +0900

    feat: 로그인 block 기능 추가

--- frontend/src/components/LoginPage/SocialKakao.jsx
+++ frontend/src/components/LoginPage/SocialKakao.jsx
@@ -1,4 +1,6 @@
 import kakaoSymbol from "/images/kakao_symbol.png";
+import { useRecoilState } from "recoil";
+import { modalAlertState } from "@states//ModalState";
 
 const SocialKakao = () => {
   const Rest_api_key = import.meta.env.VITE_KAKAO_REST_API_KEY; //REST API KEY
@@ -6,13 +8,25 @@ const SocialKakao = () => {
   const redirect_uri = "https://boongobbang.site/login/oauth2/code/kakao";
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  
+  const [alertOpen, setAlertOpen] = useRecoilState(modalAlertState);
+
+  // 로그인 막는 함수, 이후 onClick={handleLogin}으로 변경
+  const blockLogin = () => {
+    setAlertOpen({isOpen: true, message: "2024년도 12월에 만나요🤗"})
+    setTimeout(() => {
+      setAlertOpen({ isOpen: false, message: "" });
+    }, 3000);
+  }
+  
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
+
   return (
     <div
       className="relative mb-[2%] flex h-[50px] w-[70%] items-center justify-center rounded-[12px] bg-[#FEE500] text-center font-sans font-sans text-[16px] text-[#191919] min-[400px]:text-[20px] min-[500px]:text-[22px] min-[600px]:text-[25px]"
-      onClick={handleLogin}
+      onClick={blockLogin}
     >
       <img
         className="absolute left-[10%] h-[28px] w-[28px] min-[400px]:h-[30px] min-[400px]:w-[30px]"
